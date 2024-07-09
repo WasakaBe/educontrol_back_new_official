41b6c48a3f1d3a50032d76595cdb82445e0a0a8d
Wasaka BegeinTv <127060500+WasakaBe@users.noreply.github.com>
2024-07-09 09:25:06 -0600

    Update setup.sh

--- .render/setup.sh
+++ .render/setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # Instalar dependencias del sistema
 apt-get update && apt-get install -y python3-dev build-essential
+apt-get install -y curl apt-transport-https
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
 
 # Crear un entorno virtual
 python3 -m venv venv
