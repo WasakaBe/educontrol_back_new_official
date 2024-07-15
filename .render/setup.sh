53fb592eeaa1a8a0b71529f5ce3ef1cb8e683d02
Wasaka BegeinTv <127060500+WasakaBe@users.noreply.github.com>
2024-07-14 21:59:07 -0600

    Update setup.sh

--- .render/setup.sh
+++ .render/setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# Instalar dependencias del sistema
+# Actualizar los paquetes y instalar dependencias del sistema
 apt-get update && apt-get install -y python3-dev build-essential
 apt-get install -y curl apt-transport-https
+
+# Añadir la clave y el repositorio de Microsoft para el controlador ODBC 17
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+# Actualizar los paquetes nuevamente y instalar el controlador ODBC 17
 apt-get update
 ACCEPT_EULA=Y apt-get install -y msodbcsql17
 
@@ -15,5 +19,3 @@ source venv/bin/activate
 
 # Instalar dependencias de Python
 pip install -r requirements.txt
-
-.render/setup.sh
